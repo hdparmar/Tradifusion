8e544557abe3b43f73314a2a48d0b6afb8e69973
Harshdeep Parmar <71045029+hdparmar@users.noreply.github.com>
2023-08-17 23:59:19 +0200

    Trainer Class for Model

---
+++ trainerClass.py
@@ -0,0 +1,128 @@
+
+import tensorflow as tf
+import tensorflow.experimental.numpy as tnp
+
+class Trainer(tf.keras.Model):
+    # Reference:
+    # https://github.com/huggingface/diffusers/blob/main/examples/text_to_image/train_text_to_image.py
+
+    def __init__(
+        self,
+        diffusion_model,
+        vae,
+        noise_scheduler,
+        use_mixed_precision=False,
+        max_grad_norm=1.0,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.diffusion_model = diffusion_model
+        self.vae = vae
+        self.noise_scheduler = noise_scheduler
+        self.max_grad_norm = max_grad_norm
+
+        self.use_mixed_precision = use_mixed_precision
+        self.vae.trainable = False
+
+    def train_step(self, inputs):
+        images = inputs["images"]
+        encoded_text = inputs["encoded_text"]
+        batch_size = tf.shape(images)[0]
+
+        with tf.GradientTape() as tape:
+            # Project image into the latent space and sample from it.
+            latents = self.sample_from_encoder_outputs(self.vae(images, training=False))
+            # Know more about the magic number here:
+            # https://keras.io/examples/generative/fine_tune_via_textual_inversion/
+            latents = latents * 0.18215
+
+            # Sample noise that we'll add to the latents.
+            noise = tf.random.normal(tf.shape(latents))
+
+            # Sample a random timestep for each image.
+            timesteps = tnp.random.randint(
+                0, self.noise_scheduler.train_timesteps, (batch_size,)
+            )
+
+            # Add noise to the latents according to the noise magnitude at each timestep
+            # (this is the forward diffusion process).
+            noisy_latents = self.noise_scheduler.add_noise(
+                tf.cast(latents, noise.dtype), noise, timesteps
+            )
+
+            # Get the target for loss depending on the prediction type
+            # just the sampled noise for now.
+            target = noise  # noise_schedule.predict_epsilon == True
+
+            # Predict the noise residual and compute loss.
+            timestep_embedding = tf.map_fn(
+                lambda t: self.get_timestep_embedding(t), timesteps, fn_output_signature=tf.float32
+            )
+            timestep_embedding = tf.squeeze(timestep_embedding, 1)
+            model_pred = self.diffusion_model(
+                [noisy_latents, timestep_embedding, encoded_text], training=True
+            )
+            loss = self.compiled_loss(target, model_pred)
+            if self.use_mixed_precision:
+                loss = self.optimizer.get_scaled_loss(loss)
+
+        # Update parameters of the diffusion model.
+        trainable_vars = self.diffusion_model.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        if self.use_mixed_precision:
+            gradients = self.optimizer.get_unscaled_gradients(gradients)
+        gradients = [tf.clip_by_norm(g, self.max_grad_norm) for g in gradients]
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        return {m.name: m.result() for m in self.metrics}
+    
+    def test_step(self, inputs):
+        images = inputs["images"]
+        encoded_text = inputs["encoded_text"]
+        batch_size = tf.shape(images)[0]
+
+        latents = self.sample_from_encoder_outputs(self.vae(images, training=False))
+        latents = latents * 0.18215
+        noise = tf.random.normal(tf.shape(latents))
+        timesteps = tnp.random.randint(0, self.noise_scheduler.train_timesteps, (batch_size,))
+        noisy_latents = self.noise_scheduler.add_noise(tf.cast(latents, noise.dtype), noise, timesteps)
+        target = noise
+        timestep_embedding = tf.map_fn(lambda t: self.get_timestep_embedding(t), timesteps, fn_output_signature=tf.float32)
+        timestep_embedding = tf.squeeze(timestep_embedding, 1)
+        model_pred = self.diffusion_model([noisy_latents, timestep_embedding, encoded_text], training=False)
+    
+        # Use compiled_loss
+        loss = self.compiled_loss(target, model_pred)
+    
+        return {'loss': loss}
+
+    def get_timestep_embedding(self, timestep, dim=320, max_period=10000):
+        half = dim // 2
+        log_max_preiod = tf.math.log(tf.cast(max_period, tf.float32))
+        freqs = tf.math.exp(
+            -log_max_preiod * tf.range(0, half, dtype=tf.float32) / half
+        )
+        args = tf.convert_to_tensor([timestep], dtype=tf.float32) * freqs
+        embedding = tf.concat([tf.math.cos(args), tf.math.sin(args)], 0)
+        embedding = tf.reshape(embedding, [1, -1])
+        return embedding
+
+    def sample_from_encoder_outputs(self, outputs):
+        mean, logvar = tf.split(outputs, 2, axis=-1)
+        logvar = tf.clip_by_value(logvar, -30.0, 20.0)
+        std = tf.exp(0.5 * logvar)
+        sample = tf.random.normal(tf.shape(mean), dtype=mean.dtype)
+        return mean + std * sample
+
+    def save_weights(self, filepath, overwrite=True, save_format=None, options=None):
+        # Overriding this method will allow us to use the `ModelCheckpoint`
+        # callback directly with this trainer class. In this case, it will
+        # only checkpoint the `diffusion_model` since that's what we're training
+        # during fine-tuning.
+        self.diffusion_model.save_weights(
+            filepath=filepath,
+            overwrite=overwrite,
+            save_format=save_format,
+            options=options,
+        )
